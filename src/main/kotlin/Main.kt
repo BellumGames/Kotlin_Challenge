e90921264b5be22861b24fd9902e116d79a88bc4
BELLUM_GAMES <79925845+BellumGames@users.noreply.github.com>
2022-11-16 16:20:51 +0200

    Problem 2 was resolved!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
 fun main() {
     val intArray: Array<Int> = arrayOf(6,9,24)
     println(problem1(intArray))
+
+    val charArray: Array<Char> = arrayOf('a','b','c','e')
+    println(problem2(charArray))
 }
 
 // Given an integer array of size N, compute the sum of all even numbers in this array
@@ -11,4 +14,31 @@ fun problem1(intArray: Array<Int>) : Int {
             sum += index
     }
     return sum
+}
+
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+// You will always get a valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.(Use the English alphabet with 26 letters!)
+fun problem2(charArray: Array<Char>) : String{
+    if(charArray.size < 2)
+        return "Dude, your array is too short try something longer :)"
+    var upperCaseOn = true
+    if(charArray[0].code in 97..122)
+        upperCaseOn = false
+    else if(charArray[0].code in 65..90)
+        upperCaseOn = true
+    for(i in 1 until charArray.size) {
+        val previous:Int = charArray[i - 1].code
+        val current:Int = charArray[i].code
+        if(current - previous != 1){
+            if(upperCaseOn)
+                if(previous > current)
+                    return "Letter " + (previous + 1).toChar().toString() + " is missing!"
+            if(!upperCaseOn)
+                if(previous < current)
+                    return "Letter " + (previous + 1).toChar().toString() + " is missing!"
+            return "Different cases detected!"
+        }
+    }
+    return "Not a single letter of the alphabet is missing in this array!"
 }
--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -17,4 +17,64 @@ class Test {
     }
 
     //---------------------------------------------------------------------------------------------------------------
+
+    @org.junit.jupiter.api.Test
+    internal fun testProblem2_Testcase_TooShortArray() {
+        val charArray: Array<Char> = arrayOf('a')
+        val expected = "Dude, your array is too short try something longer :)"
+        Assertions.assertEquals(expected,problem2(charArray))
+    }
+
+    @org.junit.jupiter.api.Test
+    internal fun testProblem2_Testcase_DWillHaveToBeMissing() {
+        val charArray: Array<Char> = arrayOf('a','b','c','e')
+        val expected = "Letter d is missing!"
+        Assertions.assertEquals(expected,problem2(charArray))
+    }
+
+    @org.junit.jupiter.api.Test
+    internal fun testProblem2_Testcase_DifferentCasePattern_Upper() {
+        val charArray: Array<Char> = arrayOf('A','b','C','D')
+        val expected = "Different cases detected!"
+        Assertions.assertEquals(expected,problem2(charArray))
+    }
+
+    @org.junit.jupiter.api.Test
+    internal fun testProblem2_Testcase_DifferentCasePattern_Lower() {
+        val charArray: Array<Char> = arrayOf('a','B','c','d')
+        val expected = "Different cases detected!"
+        Assertions.assertEquals(expected,problem2(charArray))
+    }
+
+    @org.junit.jupiter.api.Test
+    internal fun testProblem2_Testcase_NoLetterMissing_Upper() {
+        val charArray: Array<Char> = arrayOf('A','B','C','D')
+        val expected = "Not a single letter of the alphabet is missing in this array!"
+        Assertions.assertEquals(expected,problem2(charArray))
+    }
+
+    @org.junit.jupiter.api.Test
+    internal fun testProblem2_Testcase_NoLetterMissing_Lower() {
+        val charArray: Array<Char> = arrayOf('a','b','c','d')
+        val expected = "Not a single letter of the alphabet is missing in this array!"
+        Assertions.assertEquals(expected,problem2(charArray))
+    }
+
+    @org.junit.jupiter.api.Test
+    internal fun testProblem2_Testcase_WeDon_tStartAtA_NoLetterMissing() {
+        val charArray: Array<Char> = arrayOf('b','c','d','e')
+        val expected = "Not a single letter of the alphabet is missing in this array!"
+        Assertions.assertEquals(expected,problem2(charArray))
+    }
+
+    @org.junit.jupiter.api.Test
+    internal fun testProblem2_Testcase_WeDon_tStartAtA_LetterMissing() {
+        val charArray: Array<Char> = arrayOf('b','c','d','f')
+        val expected = "Letter e is missing!"
+        Assertions.assertEquals(expected,problem2(charArray))
+    }
+
+    //---------------------------------------------------------------------------------------------------------------
+
+
 }
