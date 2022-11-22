f311edc5c0368b9a42010570ffaaa2cd1711c744
BELLUM_GAMES <79925845+BellumGames@users.noreply.github.com>
2022-11-22 13:29:59 +0200

    Update problem 1 for list implementation using kotlin methods

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
 fun main() {
     val intArray: Array<Int> = arrayOf(6,9,24)
     println(problem1(intArray))
+
+    val intList: List<Int> = listOf(6,9,24)
+    println(problem1(intList))
 }
 
 // Given an integer array of size N, compute the sum of all even numbers in this array
@@ -11,4 +14,8 @@ fun problem1(intArray: Array<Int>) : Int {
             sum += index
     }
     return sum
+}
+
+fun problem1(intList: List<Int>) : Int {
+    return intList.sumOf { if(it % 2 == 0) it else 0}
 }
--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Assertions
-import java.util.*
 
 class Test {
     @org.junit.jupiter.api.Test
@@ -12,6 +11,19 @@ class Test {
     @org.junit.jupiter.api.Test
     internal fun testProblem1_Testcase_OddNumbersOnlyShouldGive0() {
         val intArray: Array<Int>  = arrayOf(9,1)
+        val expected = 0
+        Assertions.assertEquals(expected, problem1(intArray))
+    }
+    @org.junit.jupiter.api.Test
+    internal fun testProblem1_Testcase_EvenNumberWillGetSumOnly_ListImplementation() {
+        val intArray: List<Int> = listOf(6,9,24)
+        val expected = 30
+        Assertions.assertEquals(expected, problem1(intArray))
+    }
+
+    @org.junit.jupiter.api.Test
+    internal fun testProblem1_Testcase_OddNumbersOnlyShouldGive0_ListImplementation() {
+        val intArray: List<Int>  = listOf(9,1)
         val expected = 0
         Assertions.assertEquals(expected, problem1(intArray))
     }
